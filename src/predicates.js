d7a721d0ff728c2119251276a54fccac826a29bf
Shawn Allen <shawn.allen@github.com>
2017-10-19 11:27:13 -0700

    clean up predicates, add count()

--- src/predicates.js
+++ src/predicates.js
@@ -1,11 +1,30 @@
 const is = require('unist-util-is')
 const select = require('unist-util-select')
 
+const ascending = f => (a, b) => cmp(f(a), f(b))
+const descending = f => (a, b) => cmp(f(b), f(a))
+const cmp = (a, b) => (a > b ? 1 : a < b ? -1 : 0)
+
+const find = (node, selector) => {
+  selector = selector.trim()
+  // see: <https://developer.mozilla.org/en-US/docs/Web/CSS/:has>
+  // ':has(> ident)' should only return direct descendants (children)
+  // of type 'ident'
+  if (selector.charAt(0) === '>') {
+    selector = selector.replace(/^>\s*/, '')
+    return node.children
+      ? node.children.filter(child => is(selector, child))
+      : []
+  } else {
+    return select(node, selector)
+  }
+}
 
 const predicates = {
-  has: selector => node => select(node, selector).length > 0,
-  lacks: selector => node => select(node, selector).length === 0,
-  not: selector => node => !is(selector, node),
+  has:    selector => node => find(node, selector).length > 0,
+  lacks:  selector => node => find(node, selector).length === 0,
+  not:    selector => node => !is(selector, node),
+  count:  (selector, count) => node => find(node, selector).length == count,
 }
 
 const some = tests => d => tests.some(test => test(d))
@@ -23,21 +42,19 @@ const createFilter = filters => {
   const tests = Object.keys(predicates)
     .filter(key => filters[key])
     .map(key => predicate(key, filters[key]))
-  return tests.length ? every(tests) : identity(true)
+  return tests.length
+    ? every(tests)
+    : identity(true)
 }
-
-const ascending = f => (a, b) => cmp(f(a), f(b))
-const descending = f => (a, b) => cmp(f(b), f(a))
-const cmp = (a, b) => (a > b ? 1 : a < b ? -1 : 0)
 
 module.exports = {
   ascending,
-  descending,
   cmp,
   createFilter,
+  descending,
+  every,
+  identity,
   predicate,
   predicates,
   some,
-  every,
-  identity,
 }
